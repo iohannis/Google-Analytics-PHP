c7802287246aaa3b27c0168cefb1dea4cd89ab93
Calle <hunefalk@gmail.com>
2013-02-19 14:13:52 +0100

    Added config-sample.php for configuration settings.

---
+++ config-sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Configuration file for easier setup
+ **/
+ 
+$config = array(
+			'analytics'	=> array(
+				'access_type'	=>	'online',
+				'application_name'	=>	'My Application name',
+				'client_id'	=>	'INSERT HERE',
+				'client_secret'	=>	'INSERT HERE',
+				'api_key'	=>	'INSERT HERE',
+			),
+			'adsense'	=> array(
+				'access_type'	=>	'online',
+				'application_name'	=>	'My Application name',
+				'client_id'	=>	'INSERT HERE',
+				'client_secret'	=>	'INSERT HERE',
+				'api_key'	=>	'INSERT HERE',
+			),
+		);
